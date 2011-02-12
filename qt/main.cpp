c4b091e9d672432202357a9ff9ec8a7bd7c647b3
Paul O'Neil <redballoon36@gmail.com>
2011-02-12 18:33:15 -0500

    Pick a seed for the random numbers Changed indentation

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,18 +1,20 @@
 #include <boost/python.hpp>
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <stdlib.h>
 
 #ifndef NODE_TEST
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    sim::MainWindow w;
-    w.show();
-
-    Py_Initialize();
-
-    return a.exec();
+  srandom(time(NULL));
+  QApplication a(argc, argv);
+  sim::MainWindow w;
+  w.show();
+  
+  Py_Initialize();
+  
+  return a.exec();
 }
 
 #endif NODE_TEST
