be1841160353220cb04c42a7a880f57cf8af1808
Paul O'Neil <redballoon36@gmail.com>
2011-03-08 18:47:09 -0500

    Created new typedefs for Arduino protocol elements Used reinterpret_cast

--- src/arduino.cpp
+++ src/arduino.cpp
@@ -2,20 +2,20 @@
 #include "arduino.h"
 using namespace robot;
 
-void Arduino::getSensor(uint8_t id, char * value) throw(Serial::ReadError) {
+void Arduino::getSensor(sensorid_t id, char * value) throw(Serial::ReadError) {
   uint8_t buff[7];
   buff[0] = GET;
   buff[1] = id;
   buff[2] = END;
-  serial.Write((char*)buff, 3);
-  serial.Read((char*)buff, 7);
+  serial.Write(reinterpret_cast<char*>(buff), 3);
+  serial.Read(reinterpret_cast<char*>(buff), 7);
   if( buff[0] != GET || buff[1] != id || buff[7] != END ) {
     throw Serial::ReadError();
   }
   memcpy(value, buff+2, 4);
 }
 
-void Arduino::setMotor(uint8_t id, int32_t value) throw(Serial::WriteError) {
+void Arduino::setMotor(motorid_t id, int32_t value) throw(Serial::WriteError) {
   uint8_t buff[7];
   buff[0] = SET_MOTOR;
   buff[1] = id;
@@ -24,15 +24,15 @@ void Arduino::setMotor(uint8_t id, int32_t value) throw(Serial::WriteError) {
   buff[4] = ( value >> 16) & 0xFF;
   buff[5] = ( value >> 24) & 0xFF;
   buff[6] = END;
-  serial.Write((char*)buff, 7);
-  serial.Read((char*)buff, 3);
+  serial.Write(reinterpret_cast<char*>(buff), 7);
+  serial.Read(reinterpret_cast<char*>(buff), 3);
   if( buff[0] != SET_MOTOR || buff[1] != id || buff[2] != END ) {
     throw Serial::WriteError();
   }
 }
 
-void Arduino::setSensor(uint8_t id, int32_t value) throw(Serial::WriteError) {
-  char buff[7];
+void Arduino::setSensor(sensorid_t id, int32_t value) throw(Serial::WriteError) {
+  uint8_t buff[7];
   buff[0] = SET_SENSOR;
   buff[1] = id;
   buff[2] = value & 0xFF;
@@ -40,8 +40,8 @@ void Arduino::setSensor(uint8_t id, int32_t value) throw(Serial::WriteError) {
   buff[4] = ( value >> 16) & 0xFF;
   buff[5] = ( value >> 24) & 0xFF;
   buff[6] = END;
-  serial.Write(buff, 7);
-  serial.Read(buff, 3);
+  serial.Write(reinterpret_cast<char*>(buff), 7);
+  serial.Read(reinterpret_cast<char*>(buff), 3);
   if( buff[0] != SET_SENSOR || buff[1] != id || buff[2] != END ) {
     throw Serial::WriteError();
   }
--- src/arduino.h
+++ src/arduino.h
@@ -9,6 +9,11 @@
 #define __ARDUINO_H__
 
 namespace robot {
+  
+  typedef uint8_t id_t;
+  typedef id_t motorid_t;
+  typedef id_t sensorid_t;
+  
   class Arduino {
   protected:
     Serial serial;
@@ -22,13 +27,13 @@ namespace robot {
       LIGHT_SWITCH = 0x04,
     };
     
-    static const int MOTOR_FLAG = 0x80;
+    static const motorid_t MOTOR_FLAG = 0x80;
     static const uint8_t END = 0xFF;
     
     void setup();
-    void getSensor(uint8_t id, char * value) throw(Serial::ReadError);
-    void setMotor(uint8_t id, int32_t value) throw(Serial::WriteError);
-    void setSensor(uint8_t id, int32_t value) throw(Serial::WriteError);
+    void getSensor(sensorid_t id, char * value) throw(Serial::ReadError);
+    void setMotor(motorid_t id, int32_t value) throw(Serial::WriteError);
+    void setSensor(sensorid_t id, int32_t value) throw(Serial::WriteError);
     void switchLight(bool on);
   };
 }
