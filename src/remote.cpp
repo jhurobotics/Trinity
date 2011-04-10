8defc4b0aa9f988c1c5011e26857306e89b4145f
elan <elan@sensorstar.com>
2011-04-10 00:05:20 -0400

    updated remote class

--- src/remote.cpp
+++ src/remote.cpp
@@ -17,17 +17,33 @@ int main(){
   Arduino *ard = new Arduino();
   ard->setup("/dev/ttyACM0");
   int sensorId = 0;
-  float sensorVal;
+  int encoderVal;
+  float sonarVal;
   for(;;){
+
+		std::cout<< "\n Please enter a sensor to query. (1-2 for encoder, 3-6 sonars " << std::endl;
     std::cin >> sensorId;
     
-    try{
-      ard->getSensor(sensorId, &sensorVal);
-    }catch(...){
-      std::cout<< "Read Error! :(" << std::endl;
-    }
-    
-    std::cout << "Sensor " << sensorId<< " just read " << sensorVal <<std::endl;
+		if(sensorId == 1 || sensorId == 2){
+			try{
+		    ard->getSensor(sensorId, &encoderVal);
+		  }catch(...){
+		    std::cout<< "Read Error! :(" << std::endl;
+		  }
+
+	    std::cout << "Sensor (encoder) " << sensorId<< " just read " << encoderVal <<std::endl;
+		}else if(sensorId == 3 || sensorId == 4 || sensorId == 5 || sensorId == 6){
+		  try{
+		    ard->getSensor(sensorId, &sonarVal);
+		  }catch(...){
+		    std::cout<< "Read Error! :(" << std::endl;
+		  }
+
+	    std::cout << "Sensor (sonar) " << sensorId<< " just read " << sonarVal <<std::endl;
+		}else{
+			std::cout << "Invalid sensor ID" <<std::endl;
+		}
+
   }
   return 0;
 }
