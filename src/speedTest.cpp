050135c77a26053d3ad944d49e73d3159e955c04
Paul O'Neil <redballoon36@gmail.com>
2011-04-22 18:59:23 -0400

    Add a short delay after the button press in speed test to allow the physical button to go up

--- src/speedTest.cpp
+++ src/speedTest.cpp
@@ -3,6 +3,7 @@
  */
 #include "arduino.h"
 #include "speedTest.h"
+#include "timers.h"
 using namespace robot;
 #include <iostream>
 using namespace std;
@@ -12,6 +13,8 @@ void SpeedTest::act() {
   arduino->buttonWait();
   cout << "going!\n";
   control->setVelocity(10);
+  struct timeval start = time();
+  while( time_diff(start, time()) < 0.25 ) ;
   arduino->buttonWait();
   cout << "stopping!\n";
   halt();
